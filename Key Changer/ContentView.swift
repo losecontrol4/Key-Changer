b2986e86a8041cdcb094bf9d78d1bf8dd1f4e193
Adam Hall <arhall21@g.holycross.edu>
2023-02-15 12:42:09 -0500

    Initial Commit

---
+++ Key Changer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Key Changer
+//
+//  Created by Adam Hall on 2/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Key Changer/Key_ChangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Key_ChangerApp.swift
+//  Key Changer
+//
+//  Created by Adam Hall on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Key_ChangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
